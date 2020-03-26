0267f007126ae74077b2a2341fed6871189cafab
10266499CC <62706612+10266499CC@users.noreply.github.com>
2020-03-26 15:33:19 +0000

    Add files via upload
    Uploaded .py file

---
+++ 10266499 CA 1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# This data is the 2018 PISA Results for Reading
+# It was taken from http://factsmaps.com/pisa-2018-worldwide-ranking-average-score-of-mathematics-science-reading/
+
+
