c1f2fd01d96aece7d7e98abb1c30c181cfaf4157
10266499CC <62706612+10266499CC@users.noreply.github.com>
2020-03-26 15:37:38 +0000

    Update 10266499 CA 1.py

--- 10266499 CA 1.py
+++ 10266499 CA 1.py
@@ -4,7 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-# This data is the 2018 PISA Results for Reading
+# This data is the results of the 2018 PISA Reading Assessment 
 # It was taken from http://factsmaps.com/pisa-2018-worldwide-ranking-average-score-of-mathematics-science-reading/
 
 
