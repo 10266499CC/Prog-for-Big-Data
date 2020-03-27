16afb95f36cc5a732e5db049721fa3188f122566
10266499CC <62706612+10266499CC@users.noreply.github.com>
2020-03-27 14:11:23 +0000

    Update 10266499 CA 1.py
    Converted the website's cURL to python code using https://curl.trillworks.com/

--- 10266499 CA 1.py
+++ 10266499 CA 1.py
@@ -7,6 +7,28 @@ This is a temporary script file.
 # This a list of all of the Oscar winning movies, from 1927 to 2019 inclusive.  
 # It was taken from https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films
 
+# Converted the cURL command to python code using https://curl.trillworks.com/
+# The code below contains the python code from https://curl.trillworks.com/
+
+import requests
+
+headers = {
+    'authority': 'en.wikipedia.org',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'sec-fetch-dest': 'document',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'referer': 'https://www.google.com/',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cookie': 'WMF-Last-Access=27-Mar-2020; WMF-Last-Access-Global=27-Mar-2020; GeoIP=IE:L:Athlone:53.42:-7.94:v4; enwikimwuser-sessionId=1ecc224ef612e566c51c',
+    'if-modified-since': 'Tue, 17 Mar 2020 01:16:16 GMT',
+}
+
+response = requests.get('https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films', headers=headers)
 
 
 
